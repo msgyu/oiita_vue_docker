971bfb17a73488165ccbc328a8b0c3166babe4ba
nousi <nousidayo@gmail.com>
2021-02-02 18:56:16 +0900

    web.phpを更新：layoutsの書き間違えを修正

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/{any}', function () {
-    return view('app');
-})->where('layouts.any', '.*');
+    return view('layouts.app');
+})->where('any', '.*');
 
 Auth::routes();
 
